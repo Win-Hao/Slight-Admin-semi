c14371a7239ef8eea874a2ac01a6a0cf45ac3893
Win-Hao <149796298+Win-Hao@users.noreply.github.com>
2024-10-14 15:36:34 +0000

    Deploying to gh-pages from @ Win-Hao/Slight-Admin-semi@595a1f6305b15a7c85260328f4b5ce1bcc51bcd9 🚀

---
+++ assets/about.lazy-B84Au-9w.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./index-DFwSm7fq.js";const n=o("/about")({component:e});function e(){return t.jsx("div",{className:"p-2",children:"Hello from About!"})}export{n as Route};
---
+++ assets/index.lazy-BHIQeo6m.js
@@ -0,0 +1,1 @@
+import{c as o,j as e}from"./index-DFwSm7fq.js";const l=o("/blog/")({component:()=>e.jsx("div",{children:"Hello blog!"})});export{l as Route};
