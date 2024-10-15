0e01c4c4b7befc47343be0558dd0d66d79b4e011
Win-Hao <149796298+Win-Hao@users.noreply.github.com>
2024-10-15 01:00:33 +0000

    Deploying to gh-pages from @ Win-Hao/Slight-Admin-semi@a3aeb2cc1af0db1f327ade23f4d1fb16897aea9e 🚀

--- assets/about.lazy-B84Au-9w.js
+++
@@ -1,1 +0,0 @@
-import{c as o,j as t}from"./index-DFwSm7fq.js";const n=o("/about")({component:e});function e(){return t.jsx("div",{className:"p-2",children:"Hello from About!"})}export{n as Route};
---
+++ assets/about.lazy-rTpKFaWW.js
@@ -0,0 +1,1 @@
+import{c as o,j as t}from"./index-Cx-0hsZ-.js";const n=o("/about")({component:e});function e(){return t.jsx("div",{className:"p-2",children:"Hello from About!"})}export{n as Route};
