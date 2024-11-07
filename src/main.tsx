164c99b3358c0c7fd6dfba174fe1d7ec74a05cae
Slight_wind <758494478@qq.com>
2024-11-07 10:33:51 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import './theme/tailwind.css'
 import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import './theme/index.css'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
-    // <StrictMode>
+<StrictMode>
     <App/>
-    // </StrictMode>,
+</StrictMode>,
 )
